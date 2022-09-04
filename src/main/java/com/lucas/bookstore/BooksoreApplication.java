0bec4867f189bbe852d1d3c2e841ebd15da2b0d9
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2022-09-04 20:30:07 -0300

    first commit

---
+++ src/main/java/com/lucas/bookstore/BooksoreApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksoreApplication.class, args);
+	}
+
+}
