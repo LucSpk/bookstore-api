a53f45362069587d23a793d7e788e01b8e156ad9
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2022-09-04 23:38:41 -0300

    perfil de teste implementado

--- src/main/java/com/lucas/bookstore/BooksoreApplication.java
+++ src/main/java/com/lucas/bookstore/BooksoreApplication.java
@@ -1,41 +1,14 @@
 package com.lucas.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.lucas.bookstore.domain.Categoria;
-import com.lucas.bookstore.domain.Livro;
-import com.lucas.bookstore.repositories.CategoriaRepository;
-import com.lucas.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BooksoreApplication implements CommandLineRunner{
+public class BooksoreApplication {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(BooksoreApplication.class, args);
-		
-	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informatica", "Livros de Ti");
-		
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));	
-		
 	}
 
 }
