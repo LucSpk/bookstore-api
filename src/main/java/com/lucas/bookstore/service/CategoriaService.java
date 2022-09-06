9376cc255ae1e0096b6a01d39e67f5b5be1cb086
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2022-09-06 00:04:51 -0300

    UPDATE para categoria

--- src/main/java/com/lucas/bookstore/service/CategoriaService.java
+++ src/main/java/com/lucas/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucas.bookstore.domain.Categoria;
+import com.lucas.bookstore.dtos.CategoriaDTO;
 import com.lucas.bookstore.repositories.CategoriaRepository;
 import com.lucas.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);				// Deixa o id null para evitar que o JPA atualize os dados do banco caso o ID ja exista
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
