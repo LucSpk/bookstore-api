ecdb29fe0f71deee524bec91b13599ad77207d8d
Oliveira, Lucas Alves de <olalvezluc@gmail.com>
2022-09-10 21:17:58 -0300

    Listando todos os livros por categoria

--- src/main/java/com/lucas/bookstore/service/LivroService.java
+++ src/main/java/com/lucas/bookstore/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.lucas.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,17 @@ public class LivroService {
 	@Autowired 			// Informa para o Spring que é o Spring quem vai gerenciar essa instancia de LivroRepository
 	private LivroRepository repository;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return repository.findAllByCategoria(id_cat);
 	}
 }
